1a5aa4798c1f87198f7e8f86d4909352f08b80fb
Poberezhnyk Darya Vitalyivna <114872190+DaryaPoberezhnyk@users.noreply.github.com>
2023-08-22 22:54:50 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,261 +1,124 @@
-using System.Globalization;
+﻿using System.Globalization;
 using System.Text;
+using System.Linq;
 
 namespace C_
 {
-    internal class Program
+    public interface ICalc
     {
-        public class Money
+        int Less(int valueToCompare);
+        int Greater(int valueToCompare);
+    }
+
+    public class Array : ICalc
+    {
+        private int[] numbers;
+
+        public Array(int[] numbers)
         {
-            private int dollars;
-            private int cents;
-
-            public int Dollars
-            {
-                get { return dollars; }
-                set { dollars = value; }
-            }
-
-            public int Cents
-            {
-                get { return cents; }
-                set { cents = value; }
-            }
-
-            public void DisplayAmount()
-            {
-                Console.WriteLine($"Amount: {dollars}.{cents.ToString().PadLeft(2, '0')}");
-            }
+            this.numbers = numbers;
         }
 
-        public class Product : Money
+        public int Less(int valueToCompare)
         {
-            public void DecreasePrice(int amount)
+            int count = 0;
+            foreach (int number in numbers)
             {
-                if (Cents >= amount)
-                    Cents -= amount;
-                else
+                if (number < valueToCompare)
+                    count++;
+            }
+            return count;
+        }
+
+        public int Greater(int valueToCompare)
+        {
+            int count = 0;
+            foreach (int number in numbers)
+            {
+                if (number > valueToCompare)
+                    count++;
+            }
+            return count;
+        }
+    }
+
+    interface IOutput2
+    {
+        void ShowEven();
+        void ShowOdd();
+    }
+
+    class ArrayContainer : IOutput2
+    {
+        private int[] data;
+
+        public ArrayContainer(int[] array)
+        {
+            data = array;
+        }
+
+        public void ShowEven()
+        {
+            Console.WriteLine("Even numbers:");
+            foreach (int num in data)
+            {
+                if (num % 2 == 0)
                 {
-                    int remainingAmount = amount - Cents;
-                    Cents = 100 - remainingAmount;
-                    Dollars--;
+                    Console.WriteLine(num);
                 }
             }
         }
 
-        public class Device
+        public void ShowOdd()
         {
-            protected string name;
-            protected string characteristics;
-
-            public Device(string name, string characteristics)
+            Console.WriteLine("Odd numbers:");
+            foreach (int num in data)
             {
-                this.name = name;
-                this.characteristics = characteristics;
+                if (num % 2 != 0)
+                {
+                    Console.WriteLine(num);
+                }
             }
-
-            public virtual void Sound()
-            {
-                Console.WriteLine("Device sound");
-            }
-
-            public void Show()
-            {
-                Console.WriteLine($"Device name: {name}");
-            }
-
-            public void Desc()
-            {
-                Console.WriteLine($"Device description: {characteristics}");
-            }
-        }
-
-        public class Kettle : Device
-        {
-            public Kettle(string name, string characteristics) : base(name, characteristics)
-            {
-            }
-
-            public override void Sound()
-            {
-                Console.WriteLine("Kettle sound: Whistle");
-            }
-        }
-
-        public class Microwave : Device
-        {
-            public Microwave(string name, string characteristics) : base(name, characteristics)
-            {
-            }
-
-            public override void Sound()
-            {
-                Console.WriteLine("Microwave sound: Beep");
-            }
-        }
-
-        public class Car : Device
-        {
-            public Car(string name, string characteristics) : base(name, characteristics)
-            {
-            }
-
-            public override void Sound()
-            {
-                Console.WriteLine("Car sound: Engine noise");
-            }
-        }
-
-        public class Steamship : Device
-        {
-            public Steamship(string name, string characteristics) : base(name, characteristics)
-            {
-            }
-
-            public override void Sound()
-            {
-                Console.WriteLine("Steamship sound: Steam whistle");
-            }
-        }
-
-        public class MusicalInstrument
-        {
-            protected string name;
-            protected string characteristics;
-
-            public MusicalInstrument(string name, string characteristics)
-            {
-                this.name = name;
-                this.characteristics = characteristics;
-            }
-
-            public virtual void Sound()
-            {
-                Console.WriteLine("Musical instrument sound");
-            }
-
-            public void Show()
-            {
-                Console.WriteLine($"Musical instrument name: {name}");
-            }
-
-            public void Desc()
-            {
-                Console.WriteLine($"Musical instrument description: {characteristics}");
-            }
-
-            public virtual void History()
-            {
-                Console.WriteLine("No specific history available");
-            }
-        }
-
-        public class Violin : MusicalInstrument
-        {
-            public Violin(string name, string characteristics) : base(name, characteristics)
-            {
-            }
-
-            public override void Sound()
-            {
-                Console.WriteLine("Violin sound: Sustained and expressive");
-            }
-
-            public override void History()
-            {
-                Console.WriteLine("Violin history: Originated in Italy in the early 16th century");
-            }
-        }
-
-        public class Trombone : MusicalInstrument
-        {
-            public Trombone(string name, string characteristics) : base(name, characteristics)
-            {
-            }
-
-            public override void Sound()
-            {
-                Console.WriteLine("Trombone sound: Rich and brassy");
-            }
-
-            public override void History()
-            {
-                Console.WriteLine("Trombone history: Developed in the 15th century in Germany");
-            }
-        }
-
-        public class Ukulele : MusicalInstrument
-        {
-            public Ukulele(string name, string characteristics) : base(name, characteristics)
-            {
-            }
-
-            public override void Sound()
-            {
-                Console.WriteLine("Ukulele sound: Bright and cheerful");
-            }
-
-            public override void History()
-            {
-                Console.WriteLine("Ukulele history: Originated in Hawaii in the 19th century");
-            }
-        }
-
-        public class Cello : MusicalInstrument
-        {
-            public Cello(string name, string characteristics) : base(name, characteristics)
-            {
-            }
-
-            public override void Sound()
-            {
-                Console.WriteLine("Cello sound: Deep and resonant");
-            }
-
-            public override void History()
-            {
-                Console.WriteLine("Cello history: Evolved in Europe in the 17th century");
-            }
-        }
-
-public abstract class Worker
-    {
-        public abstract void Print();
-    }
-
-    public class President : Worker
-    {
-        public override void Print()
-        {
-            Console.WriteLine("President: Manages the organization, makes strategic decisions");
         }
     }
 
-    public class Security : Worker
+    public interface ICalc2
     {
-        public override void Print()
-        {
-            Console.WriteLine("Security: Ensures safety and security of the organization");
-        }
+        int CountDistinct();
+        int EqualToValue(int valueToCompare);
     }
 
-    public class Manager : Worker
+    public class Array1 : ICalc2
     {
-        public override void Print()
+        private int[] data;
+
+        public Array1(int[] values)
         {
-            Console.WriteLine("Manager: Oversees a team or department, responsible for achieving goals");
+            data = values;
+        }
+
+        public int CountDistinct()
+        {
+            HashSet<int> distinctValues = new HashSet<int>(data);
+            return distinctValues.Count;
+        }
+
+        public int EqualToValue(int valueToCompare)
+        {
+            int count = 0;
+            foreach (int value in data)
+            {
+                if (value == valueToCompare)
+                {
+                    count++;
+                }
+            }
+            return count;
         }
     }
-
-    public class Engineer : Worker
+    internal class Program
     {
-        public override void Print()
-        {
-            Console.WriteLine("Engineer: Designs and develops technical solutions");
-        }
-    }
-
-    static void Main(string[] args)
+        static void Main(string[] args)
         {
             CultureInfo culture = new CultureInfo("uk-UA");
             CultureInfo.DefaultThreadCurrentCulture = culture;
@@ -270,7 +133,6 @@ public abstract class Worker
                 Console.WriteLine(" |       1 -  case 1       |");
                 Console.WriteLine(" |       2 -  case 2       |");
                 Console.WriteLine(" |       3 -  case 3       |");
-                Console.WriteLine(" |       4 -  case 4       |");
                 Console.WriteLine("#===========================#");
                 Console.Write("Виберіть пункт меню -> ");
                 switch_on = Convert.ToInt32(Console.ReadLine());
@@ -281,82 +143,44 @@ public abstract class Worker
                 {
                     case 1:
                         {
-                            Product product = new Product();
-                            product.Dollars = 10;
-                            product.Cents = 50;
+                            int[] numbers = { 1, 5, 2, 7, 3, 9, 4, 6 };
+                            Array array = new Array(numbers);
 
-                            product.DisplayAmount();
+                            int lessCount = array.Less(5);
+                            Console.WriteLine($"Number of elements less than 5: {lessCount}");
 
-                            product.DecreasePrice(70);
-
-                            product.DisplayAmount();
+                            int greaterCount = array.Greater(5);
+                            Console.WriteLine($"Number of elements greater than 5: {greaterCount}");
 
                             Console.Write("\nНатисніть будь-яку клавішу, щоб продовжити...");
                             Console.ReadKey();
                             Console.Clear();
-
                         }
                         break;
-                        case 2:
+                    case 2:
                         {
-                            Device device = new Device("Generic Device", "Generic Characteristics");
-                            Kettle kettle = new Kettle("Electric Kettle", "Boils water for tea");
-                            Microwave microwave = new Microwave("Microwave Oven", "Heats food using microwaves");
-                            Car car = new Car("Sedan Car", "Four-wheeled motor vehicle");
-                            Steamship steamship = new Steamship("Steamship", "Powered by steam engine");
-
-                            device.Show(); 
-                            device.Desc(); 
-                            device.Sound(); 
-
-                            kettle.Show(); 
-                            kettle.Desc(); 
-                            kettle.Sound(); 
-
-                            microwave.Show();
-                            microwave.Desc();
-                            microwave.Sound(); 
-
-                            car.Show(); 
-                            car.Desc(); 
-                            car.Sound(); 
-
-                            steamship.Show();
-                            steamship.Desc();
-                            steamship.Sound();
+                            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                            ArrayContainer container = new ArrayContainer(array);
+                            container.ShowEven();
+                            container.ShowOdd();
 
                             Console.Write("\nНатисніть будь-яку клавішу, щоб продовжити...");
                             Console.ReadKey();
                             Console.Clear();
                         }
                         break;
-
-                    case 3:
+                        case 3:
                         {
-                            Violin violin = new Violin("Violin", "String instrument");
-                            Trombone trombone = new Trombone("Trombone", "Brass instrument");
-                            Ukulele ukulele = new Ukulele("Ukulele", "Plucked string instrument");
-                            Cello cello = new Cello("Cello", "String instrument");
+                            int[] values = { 1, 2, 3, 4, 5, 3, 2, 1 };
+                            Array1 array = new Array1(values);
 
-                            violin.Show();  
-                            violin.Desc();  
-                            violin.Sound(); 
-                            violin.History(); 
+                            int distinctCount = array.CountDistinct();
+                            Console.WriteLine("Distinct count: " + distinctCount);
 
-                            trombone.Show();  
-                            trombone.Desc();  
-                            trombone.Sound();  
-                            trombone.History();  
+                            int valueToCompare = 2;
+                            int equalToValueCount = array.EqualToValue(valueToCompare);
+                            Console.WriteLine("Count equal to " + valueToCompare + ": " + equalToValueCount);
 
-                            ukulele.Show();  
-                            ukulele.Desc();  
-                            ukulele.Sound();  
-                            ukulele.History();  
-
-                            cello.Show(); 
-                            cello.Desc();  
-                            cello.Sound();  
-                            cello.History();
 
                             Console.Write("\nНатисніть будь-яку клавішу, щоб продовжити...");
                             Console.ReadKey();
@@ -365,27 +189,8 @@ public abstract class Worker
                         }
                         break;
 
-                    case 4:
-                        {
-                            Worker president = new President();
-                            Worker security = new Security();
-                            Worker manager = new Manager();
-                            Worker engineer = new Engineer();
-
-                            president.Print(); 
-                            security.Print(); 
-                            manager.Print();  
-                            engineer.Print();
-
-                            Console.Write("\nНатисніть будь-яку клавішу, щоб продовжити...");
-                            Console.ReadKey();
-                            Console.Clear();
-
-                        }
-                        break;
                     default:
                         break;
-
                 }
             } while (switch_on != 0);
         }
